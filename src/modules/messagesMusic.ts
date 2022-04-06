36bd4e9ef5179589b5186a3c1539d9ffb143358b
J0kk3 <joakim_88@msn.com>
2022-04-06 10:36:08 +0200

    Update messagesMusic.ts

--- src/modules/messagesMusic.ts
+++ src/modules/messagesMusic.ts
@@ -1,5 +1,6 @@
 import { get, ref, remove, update, onValue, push } from "firebase/database";
 import { db } from "./firebaseApp";
+import { logOut } from "./logout";
 
 export class Messages
 {
@@ -14,35 +15,37 @@ export class Messages
   }
   private displayMsg(): void
   {
-    //The section containing all messages (section for each topic?)
+    //The div containing all message-divs
     const msgWrapper = document.getElementById("messagesMusic");
     //The Div containing one message
     const msgContainer = document.createElement("div") as HTMLDivElement;
     msgWrapper.append(msgContainer);
-    //Set the messages ID to the div containing the message
-    // msgContainer.id = this.id;
+    //Set the messages ID to the div containing the message & add styling
     msgContainer.classList.add(this.id, "chat-styling");
 
     //Create the userName h4 element
     const userNameElement = document.createElement("h4") as HTMLHeadElement;
     //Set the userName and the timeStamp
-    userNameElement.innerText =
-      this.timeStamp + `${this.userName} says ${this.message}`;
+    userNameElement.innerText = this.timeStamp + `${this.userName} says ${this.message}`;
     msgContainer.append(userNameElement);
 
     //Create the remove button
     const removeBtn = document.createElement("button") as HTMLButtonElement;
     removeBtn.innerText = "X";
-    msgContainer.append(removeBtn);
+    //If the logged-in user is the same as message-user; append the remove-button
+    if (this.userName == sessionStorage.getItem("user"))
+    {
+      msgContainer.append(removeBtn);
+    }
 
-    //Removebuttons event
+    //Remove button event
     removeBtn.addEventListener("click", () =>
     {
       const userName = sessionStorage.getItem("user");
-
+      //Check if the logged in user is the same as the user that posted the message
       if (userName == sessionStorage.getItem("user"))
       {
-        //Set the reference in the database
+        //Set the reference to the post in the database & remove it
         const msgRef = ref(db, "/Topics/Music/" + this.id);
         remove(msgRef);
       }
@@ -53,6 +56,7 @@ export class Messages
     document.querySelector(`.${this.id}`).remove();
   }
 }
+//Reference to the current topic in the database
 const dbRef = ref(db, "/Topics/Music/");
 let messages: Messages[] = [];
 onValue(dbRef, (snapshot) =>
@@ -63,6 +67,7 @@ onValue(dbRef, (snapshot) =>
     message.clearDOM();
   }
   messages = [];
+  //Send all the message data(key) to the database
   for (const key in messageData)
   {
     messages.push(
@@ -85,51 +90,60 @@ onValue(dbRef, (snapshot) =>
     {
       if (messageArray.length > 25)
       {
-        //Set the reference in the database
+        //Set the reference to the post in the database & remove it
         const post = ref(db, "/Topics/Music/" + index0);
         remove(post);
       }
     }
   }
+  //Check if theres too many posts
   if (messageData)
   {
     postLimiter();
   }
 });
-document.getElementById("send").addEventListener("click", (e) =>
+//If theres no logged in user, hide the "send-button"
+if (sessionStorage.getItem("user") == null || undefined || "")
 {
-  e.preventDefault();
-  const message = document.getElementById("userMessage") as HTMLInputElement;
-  const date = new Date();
-  const messageToAdd = {
-    timeStamp:
-      date.getFullYear() +
-      " " +
-      (date.getMonth() + 1) +
-      "/" +
-      date.getUTCDate() +
-      " - " +
-      date.getHours() +
-      ":" +
-      date.getMinutes() +
-      ": ",
-    name: sessionStorage.getItem("user"),
-    message: message.value,
-  };
+  document.getElementById("send").style.visibility = "hidden";
+}
+else
+{
+  //Otherwise, make the button visible
+  document.getElementById("send").style.visibility = "visible";
+  document.getElementById("send").addEventListener("click", (e) =>
+  {
+    e.preventDefault();
+    const message = document.getElementById("userMessage") as HTMLInputElement;
+    //Set the current timestamp
+    const date = new Date();
+    const messageToAdd = {
+      timeStamp:
+        date.getFullYear() +
+        " " +
+        (date.getMonth() + 1) +
+        "/" +
+        date.getUTCDate() +
+        " - " +
+        date.getHours() +
+        ":" +
+        date.getMinutes() +
+        ": ",
+      //Get  the logged-in user
+      name: sessionStorage.getItem("user"),
+      message: message.value,
+    };
 
-  const newKey: string = push(dbRef).key;
-  const newMessage = {};
-  newMessage[ newKey ] = messageToAdd;
-
-  update(dbRef, newMessage);
-});
-
+    const newKey: string = push(dbRef).key;
+    const newMessage = {};
+    newMessage[ newKey ] = messageToAdd;
+    //Update the database with the message data
+    update(dbRef, newMessage);
+  });
+}
 function scrollDown(): void
 {
   const e = document.getElementById("messagesMusic");
   e.scrollTop = e.scrollHeight;
 }
-document.getElementById("logout-button").addEventListener("click", () =>
-{
-  sessionStorage.clear();
-});
+logOut();
